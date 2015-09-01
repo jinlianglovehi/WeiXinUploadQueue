81ebc21556338254d3a36d797e330706c97c68dd
chenweihua <chenweihua@ihbaby.com>
2015-09-01 19:38:04 +0800

    监测目的

--- weitaixin/src/main/java/cn/ihealthbaby/weitaixin/ui/monitor/GuardianStateActivity.java
+++ weitaixin/src/main/java/cn/ihealthbaby/weitaixin/ui/monitor/GuardianStateActivity.java
@@ -1,15 +1,27 @@
 package cn.ihealthbaby.weitaixin.ui.monitor;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.widget.FrameLayout;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ihealthbaby.client.ApiManager;
+import cn.ihealthbaby.client.HttpClientAdapter;
+import cn.ihealthbaby.client.Result;
+import cn.ihealthbaby.client.model.AskPurposeType;
+import cn.ihealthbaby.client.model.CommonConfig;
+import cn.ihealthbaby.client.model.FeelingType;
 import cn.ihealthbaby.weitaixin.R;
 import cn.ihealthbaby.weitaixin.base.BaseActivity;
+import cn.ihealthbaby.weitaixin.library.util.ToastUtil;
+import cn.ihealthbaby.weitaixin.tools.CustomDialog;
 
 public class GuardianStateActivity extends BaseActivity {
 
@@ -20,6 +32,13 @@ public class GuardianStateActivity extends BaseActivity {
     @Bind(R.id.flGuardianPurpose) FrameLayout flGuardianPurpose;
     @Bind(R.id.flGuardianMood) FrameLayout flGuardianMood;
     @Bind(R.id.ivFooter) TextView ivFooter;
+    @Bind(R.id.tvGuardianPurposeText) TextView tvGuardianPurposeText;
+    @Bind(R.id.tvGuardianMoodText) TextView tvGuardianMoodText;
+
+
+    public List<AskPurposeType> askPurposetypes;
+    public List<FeelingType> feelingTypes;
+
 
 
     @Override
@@ -29,6 +48,23 @@ public class GuardianStateActivity extends BaseActivity {
         ButterKnife.bind(this);
 
         title_text.setText("监护状态");
+
+        final CustomDialog customDialog = new CustomDialog();
+        Dialog dialog = customDialog.createDialog1(this, "加载中...");
+        dialog.show();
+        ApiManager.getInstance().commonApi.getCommonConfig(new HttpClientAdapter.Callback<CommonConfig>() {
+            @Override
+            public void call(Result<CommonConfig> t) {
+                if (t.isSuccess()) {
+                    CommonConfig data = t.getData();
+                    askPurposetypes = data.getAskPurposetypes();
+                    feelingTypes = data.getFeelingTypes();
+                } else {
+                    ToastUtil.show(getApplicationContext(), t.getMsgMap() + "");
+                }
+                customDialog.dismiss();
+            }
+        }, getRequestTag());
     }
 
 
@@ -37,23 +73,49 @@ public class GuardianStateActivity extends BaseActivity {
         this.finish();
     }
 
+
     @OnClick(R.id.flGuardianPurpose)
     public void GuardianPurpose(FrameLayout flGuardianPurpose) {
-       MyPoPoWinGuardian myPoPoWinGuardian=new MyPoPoWinGuardian(this);
-       myPoPoWinGuardian.showAtLocation(flGuardianPurpose);
+        if (askPurposetypes == null) {
+            ToastUtil.show(getApplicationContext(), "没数据~~~");
+            return;
+        }
+        final MyPoPoWinGuardian myPoPoWinGuardian = new MyPoPoWinGuardian(this);
+        myPoPoWinGuardian.initPurposetData(askPurposetypes);
+        myPoPoWinGuardian.showAtLocation(flGuardianPurpose);
+        myPoPoWinGuardian.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                tvGuardianPurposeText.setText(askPurposetypes.get(myPoPoWinGuardian.indexPosition).getViewValue()+"");
+            }
+        });
     }
 
 
     @OnClick(R.id.flGuardianMood)
     public void GuardianMood(FrameLayout flGuardianMood) {
-        MyPoPoWinGuardian myPoPoWinGuardian=new MyPoPoWinGuardian(this);
+        if (feelingTypes == null) {
+            ToastUtil.show(getApplicationContext(), "没数据~~~");
+            return;
+        }
+        final MyPoPoWinGuardian myPoPoWinGuardian = new MyPoPoWinGuardian(this);
+        myPoPoWinGuardian.initFeelingTypeData(feelingTypes);
         myPoPoWinGuardian.showAtLocation(flGuardianMood);
+        myPoPoWinGuardian.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                tvGuardianMoodText.setText(feelingTypes.get(myPoPoWinGuardian.indexPosition).getViewValue()+"");
+            }
+        });
     }
 
 
     @OnClick(R.id.ivFooter)
     public void Footer(){
-
+        final CustomDialog customDialog = new CustomDialog();
+        Dialog dialog = customDialog.createDialog1(this, "加载中...");
+        dialog.show();
+//        ApiManager.getInstance().adviceApi.
     }
 
 
--- weitaixin/src/main/java/cn/ihealthbaby/weitaixin/ui/monitor/MyPoPoWinGuardian.java
+++ weitaixin/src/main/java/cn/ihealthbaby/weitaixin/ui/monitor/MyPoPoWinGuardian.java
@@ -15,8 +15,13 @@ import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cn.ihealthbaby.client.model.AskPurposeType;
+import cn.ihealthbaby.client.model.FeelingType;
 import cn.ihealthbaby.weitaixin.R;
 
 public class MyPoPoWinGuardian extends PopupWindow {
@@ -24,7 +29,6 @@ public class MyPoPoWinGuardian extends PopupWindow {
     protected View mContentView;
     protected Activity context;
     public ListView lvGuardianPurpose;
-    public MyGuardianPurposeAdapter myGuardianPurposeAdapter;
 
     public MyPoPoWinGuardian(Activity context, View contentView, int width, int height, boolean focusable) {
         super(contentView, width, height, focusable);
@@ -73,6 +77,12 @@ public class MyPoPoWinGuardian extends PopupWindow {
         context.getWindow().setAttributes(lp);
     }
 
+    public void disMiss(){
+        WindowManager.LayoutParams lp = MyPoPoWinGuardian.this.context.getWindow().getAttributes();
+        lp.alpha = 1.0f;
+        MyPoPoWinGuardian.this.context.getWindow().setAttributes(lp);
+        dismiss();
+    }
 
     public void showView(){
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -80,7 +90,6 @@ public class MyPoPoWinGuardian extends PopupWindow {
         this.setContentView(ppView);
         //
         lvGuardianPurpose = (ListView) ppView.findViewById(R.id.lvGuardianPurpose);
-        initData();
     }
 
     public void showAtLocation(View parent){
@@ -89,25 +98,29 @@ public class MyPoPoWinGuardian extends PopupWindow {
 
 
 
-    private void initData() {
-        myGuardianPurposeAdapter = new MyGuardianPurposeAdapter(context);
+    public void initPurposetData(List<AskPurposeType> askPurposetypes) {
+        final MyGuardianPurposeAdapter myGuardianPurposeAdapter = new MyGuardianPurposeAdapter(context);
+        myGuardianPurposeAdapter.askPurposetypes=askPurposetypes;
         lvGuardianPurpose.setAdapter(myGuardianPurposeAdapter);
         lvGuardianPurpose.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 myGuardianPurposeAdapter.isFirst = false;
-                view.setSelected(true);
+                indexPosition = position;
                 myGuardianPurposeAdapter.notifyDataSetChanged();
+                disMiss();
             }
         });
     }
 
-
+    public int indexPosition = 0;
     public class MyGuardianPurposeAdapter extends BaseAdapter {
 
-        private String[] titleTimes = new String[]{"5秒", "10秒", "15秒", "15秒", "15秒", "15秒", "15秒", "15秒", "15秒", "15秒", "15秒", "15秒", "15秒", "15秒", "15秒", "15秒", "15秒", "20秒", "25秒", "30秒"};
+        public List<AskPurposeType> askPurposetypes=new ArrayList<AskPurposeType>();
         private LayoutInflater inflater;
         public boolean isFirst = true;
+
+
 
         public MyGuardianPurposeAdapter(Context context) {
             inflater = LayoutInflater.from(context);
@@ -115,12 +128,12 @@ public class MyPoPoWinGuardian extends PopupWindow {
 
         @Override
         public int getCount() {
-            return titleTimes.length;
+            return askPurposetypes.size();
         }
 
         @Override
         public Object getItem(int position) {
-            return titleTimes[position];
+            return askPurposetypes.get(position);
         }
 
         @Override
@@ -138,8 +151,8 @@ public class MyPoPoWinGuardian extends PopupWindow {
             } else {
                 viewHolder = (ViewHolder) convertView.getTag();
             }
-            viewHolder.tvTime.setText(titleTimes[position]);
-            if (convertView.isSelected()) {
+            viewHolder.tvTime.setText(askPurposetypes.get(position).getViewValue()+"");
+            if (indexPosition==position) {
                 viewHolder.tvTime.setTextColor(context.getResources().getColor(R.color.green0));
                 viewHolder.tvState.setTextColor(context.getResources().getColor(R.color.green0));
                 viewHolder.tvState.setVisibility(View.VISIBLE);
@@ -168,6 +181,89 @@ public class MyPoPoWinGuardian extends PopupWindow {
         }
     }
 
+
+
+
+    public void initFeelingTypeData(List<FeelingType> feelingTypes) {
+        final MyGuardianFeelingTypeAdapter myGuardianFeelingTypeAdapter = new MyGuardianFeelingTypeAdapter(context);
+        myGuardianFeelingTypeAdapter.feelingTypes=feelingTypes;
+        lvGuardianPurpose.setAdapter(myGuardianFeelingTypeAdapter);
+        lvGuardianPurpose.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                myGuardianFeelingTypeAdapter.isFirst = false;
+                indexPosition = position;
+                myGuardianFeelingTypeAdapter.notifyDataSetChanged();
+                disMiss();
+            }
+        });
+    }
+
+
+    public class MyGuardianFeelingTypeAdapter extends BaseAdapter {
+
+        public List<FeelingType> feelingTypes=new ArrayList<FeelingType>();
+        private LayoutInflater inflater;
+        public boolean isFirst = true;
+
+        public MyGuardianFeelingTypeAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return feelingTypes.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return feelingTypes.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder = null;
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.item_set_system_guardian, null);
+                viewHolder = new ViewHolder(convertView);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            viewHolder.tvTime.setText(feelingTypes.get(position).getViewValue()+"");
+            if (indexPosition == position) {//convertView.isSelected()
+                viewHolder.tvTime.setTextColor(context.getResources().getColor(R.color.green0));
+                viewHolder.tvState.setTextColor(context.getResources().getColor(R.color.green0));
+                viewHolder.tvState.setVisibility(View.VISIBLE);
+            } else {
+                viewHolder.tvTime.setTextColor(context.getResources().getColor(R.color.gray9));
+                viewHolder.tvState.setTextColor(context.getResources().getColor(R.color.gray9));
+                viewHolder.tvState.setVisibility(View.INVISIBLE);
+            }
+            if (isFirst && position == 0) {
+                viewHolder.tvTime.setTextColor(context.getResources().getColor(R.color.green0));
+                viewHolder.tvState.setTextColor(context.getResources().getColor(R.color.green0));
+                viewHolder.tvState.setVisibility(View.VISIBLE);
+            }
+            return convertView;
+        }
+
+        class ViewHolder {
+            @Bind(R.id.tvTime)
+            TextView tvTime;
+            @Bind(R.id.tvState)
+            TextView tvState;
+
+            public ViewHolder(View convertView) {
+                ButterKnife.bind(this, convertView);
+            }
+        }
+    }
 
 
 }
