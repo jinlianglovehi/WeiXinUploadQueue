faa6780f271a8600a0f83b0e390e24275d0c946e
kang <kangdawei@ihbaby.com>
2015-09-07 17:19:27 +0800

    修改 上传设置 button UI

--- weitaixin/src/main/java/cn/ihealthbaby/weitaixin/ui/mine/SetSystemUploadActivity.java
+++ weitaixin/src/main/java/cn/ihealthbaby/weitaixin/ui/mine/SetSystemUploadActivity.java
@@ -1,6 +1,10 @@
 package cn.ihealthbaby.weitaixin.ui.mine;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -8,9 +12,7 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ihealthbaby.weitaixin.R;
-import cn.ihealthbaby.weitaixin.WeiTaiXinApplication;
 import cn.ihealthbaby.weitaixin.base.BaseActivity;
-import cn.ihealthbaby.weitaixin.ui.widget.SlideSwitchView;
 
 
 public class SetSystemUploadActivity extends BaseActivity {
@@ -22,8 +24,9 @@ public class SetSystemUploadActivity extends BaseActivity {
     @Bind(R.id.function)
     TextView function;
     @Bind(R.id.slide_switch_upload)
-    SlideSwitchView mSlideSwitchViewUpload;
-    //
+    ImageView mSlideSwitchViewUpload;
+
+    private SharedPreferences mySharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +34,15 @@ public class SetSystemUploadActivity extends BaseActivity {
         setContentView(R.layout.activity_set_system_upload);
 
         ButterKnife.bind(this);
-
+        mySharedPreferences = getSharedPreferences("config",
+                Activity.MODE_PRIVATE);
         title_text.setText("上传设置");
 //      back.setVisibility(View.INVISIBLE);
 
         initView();
         initListener();
     }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -45,15 +50,20 @@ public class SetSystemUploadActivity extends BaseActivity {
 
 
     private void initListener() {
-        mSlideSwitchViewUpload.setSlideListener(new SlideSwitchView.SlideListener() {
-            @Override
-            public void open() {
-                WeiTaiXinApplication.getInstance().putValue("SetSystemUpload", "1");
-            }
 
+
+        mSlideSwitchViewUpload.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void close() {
-                WeiTaiXinApplication.getInstance().putValue("SetSystemUpload", "0");
+            public void onClick(View v) {
+                boolean upload = mySharedPreferences.getBoolean("upload", true);
+                SharedPreferences.Editor editor = mySharedPreferences.edit();
+                if (!upload) {
+                    mSlideSwitchViewUpload.setImageResource(R.drawable.switch_on);
+                } else {
+                    mSlideSwitchViewUpload.setImageResource(R.drawable.switch_off);
+                }
+                editor.putBoolean("upload", !upload);
+                editor.commit();
             }
         });
     }
@@ -66,11 +76,12 @@ public class SetSystemUploadActivity extends BaseActivity {
 
 
     private void initView() {
-        String SetSystemUpload = WeiTaiXinApplication.getInstance().getValue("SetSystemUpload", "0");
-        if ("1".equals(SetSystemUpload)) {
-            mSlideSwitchViewUpload.setState(true);
+//        String SetSystemUpload = WeiTaiXinApplication.getInstance().getValue("SetSystemUpload", "0");
+        boolean upload = mySharedPreferences.getBoolean("upload", true);
+        if (upload) {
+            mSlideSwitchViewUpload.setImageResource(R.drawable.switch_on);
         } else {
-            mSlideSwitchViewUpload.setState(false);
+            mSlideSwitchViewUpload.setImageResource(R.drawable.switch_off);
         }
     }
 
