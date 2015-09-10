8351ae1c1f3693445b87c7e676c417b7e03c7ab6
kang <kangdawei@ihbaby.com>
2015-09-10 21:29:19 +0800

    'test'

--- weitaixinpro/src/main/java/cn/ihealthbaby/weitaixinpro/ui/monitor/Monitorfragment.java
+++ weitaixinpro/src/main/java/cn/ihealthbaby/weitaixinpro/ui/monitor/Monitorfragment.java
@@ -5,16 +5,32 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import cn.ihealthbaby.weitaixinpro.R;
 import cn.ihealthbaby.weitaixinpro.base.BaseFragment;
 
 /**
  * Created by liuhongjian on 15/8/12 17:52.
  */
-public class MonitorFragment extends BaseFragment {
+public class MonitorFragment extends BaseFragment implements View.OnClickListener {
 
     private static MonitorFragment instance;
+    @Bind(R.id.iv_no_monitor)
+    ImageView mIvNoMonitor;
+    @Bind(R.id.rl_no_monitor)
+    RelativeLayout mRlNoMonitor;
+    @Bind(R.id.iv_alreay_monitor)
+    ImageView mIvAlreayMonitor;
+    @Bind(R.id.rl_already_monitor)
+    RelativeLayout mRlAlreadyMonitor;
+    @Bind(R.id.lv_monitor)
+    ListView mLvMonitor;
+
 
     public static MonitorFragment getInstance() {
         if (instance == null) {
@@ -26,7 +42,39 @@ public class MonitorFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.monitor_fragment, null);
+        View view = inflater.inflate(R.layout.monitor_fragment, null);
+        ButterKnife.bind(this, view);
+        initView();
+        initListener();
+        return view;
+    }
+
+    private void initListener() {
+        mRlAlreadyMonitor.setOnClickListener(this);
+        mRlNoMonitor.setOnClickListener(this);
+    }
+
+    private void initView() {
+        mIvNoMonitor.setVisibility(View.VISIBLE);
+        mIvAlreayMonitor.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rl_already_monitor:
+
+                break;
+
+            case R.id.rl_no_monitor:
+                break;
+        }
     }
 }
 
