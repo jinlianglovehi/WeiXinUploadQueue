66c0e95a6654ff3da6fec66c153e6e7ef145c667
kang <kangdawei@ihbaby.com>
2015-09-10 11:20:41 +0800

    院内版  增加  监护常识

---
+++ weitaixinpro/src/main/java/cn/ihealthbaby/weitaixinpro/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package cn.ihealthbaby.weitaixinpro.ui.login;
+
+import android.os.Bundle;
+
+import cn.ihealthbaby.weitaixinpro.base.BaseActivity;
+
+/**
+ * @author by kang on 2015/9/10.
+ */
+public class LoginActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ weitaixinpro/src/main/java/cn/ihealthbaby/weitaixinpro/ui/monitor/MonitorCommonSense.java
@@ -0,0 +1,18 @@
+package cn.ihealthbaby.weitaixinpro.ui.monitor;
+
+import android.os.Bundle;
+
+import cn.ihealthbaby.weitaixinpro.R;
+import cn.ihealthbaby.weitaixinpro.base.BaseActivity;
+
+/**
+ * @author by kang on 2015/9/10.
+ */
+public class MonitorCommonSense extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitor_common_sence);
+    }
+}
