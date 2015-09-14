33691829c93142874c23adbba486d18ff21ebe59
liuhongjian <liuhongjian@ihealthbaby.cn>
2015-09-14 11:41:20 +0800

    增加数据库测试用例

--- weitaixin/src/androidTest/java/cn/ihealthbaby/weitaixin/db/DataDaoTest.java
+++ weitaixin/src/androidTest/java/cn/ihealthbaby/weitaixin/db/DataDaoTest.java
@@ -9,7 +9,7 @@ import cn.ihealthbaby.weitaixin.model.MyAdviceItem;
  * Created by liuhongjian on 15/9/13 16:26.
  */
 public class DataDaoTest extends AndroidTestCase {
-	public final static String JIANCEID = "1234567890 - dfghjkl";
+	public final static String JIANCEID = "012345678901213";
 	private Context context;
 	private DataDao dao;
 	private MyAdviceItem myAdviceItem;
@@ -40,8 +40,7 @@ public class DataDaoTest extends AndroidTestCase {
 	}
 
 	public void testFindNative() throws Exception {
-		dao.add(myAdviceItem,true);
-//		dao.addItem(myAdviceItem, true);
+//		dao.add(myAdviceItem,true);
 		MyAdviceItem aNative = dao.findNative(JIANCEID);
 		assertNotNull(aNative);
 		assertTrue(aNative.getJianceid().equals(myAdviceItem.getJianceid()));
@@ -50,6 +49,17 @@ public class DataDaoTest extends AndroidTestCase {
 	}
 
 	public void testUpdateItem() throws Exception {
+		myAdviceItem.setSerialnum("10010");
+		dao.update(myAdviceItem);
+		MyAdviceItem aNative = dao.findNative(JIANCEID);
+		assertNotNull(aNative);
+		assertTrue(aNative.getJianceid().equals(myAdviceItem.getJianceid()));
+		assertTrue(aNative.getFeeling().equals(myAdviceItem.getFeeling()));
+		assertTrue(aNative.getPurpose().equals(myAdviceItem.getPurpose()));
+		assertEquals(aNative.getSerialnum(), myAdviceItem.getSerialnum());
+	}
+
+	public void testUpdateOrInsert() {
 
 	}
 }
