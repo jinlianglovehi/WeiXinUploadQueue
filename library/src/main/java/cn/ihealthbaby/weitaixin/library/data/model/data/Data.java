e885b9b67df0198bb8457cbc612607fd463a27ab
liuhongjian <liuhongjian@ihealthbaby.cn>
2015-09-17 23:16:48 +0800

    调整数据结构 String->List<Long>

--- library/src/main/java/cn/ihealthbaby/weitaixin/library/data/model/data/Data.java
+++ library/src/main/java/cn/ihealthbaby/weitaixin/library/data/model/data/Data.java
@@ -1,55 +1,52 @@
 package cn.ihealthbaby.weitaixin.library.data.model.data;
 
+import java.util.List;
+
 /**
  * Created by liuhongjian on 15/9/9 21:42.
  */
 public class Data {
-	private String heartRate;
-	private String fm;
-	private String afm;
-	private String doctor;
+	private List<Integer> heartRate;
+	private List<Long> fm;
+	private List<Long> afm;
+	private List<Long> doctor;
 	private int interval;
 	private long time;
 
-	public String getHeartRate() {
+	public List<Integer> getHeartRate() {
 		return heartRate;
 	}
 
-	public void setHeartRate(String heartRate) {
-		this.heartRate = heartRate;
-	}
-
-	public String getAfm() {
+	public List<Long> getAfm() {
 		return afm;
 	}
 
-	public void setAfm(String afm) {
+	public void setAfm(List<Long> afm) {
 		this.afm = afm;
 	}
 
-	public String getDoctor() {
+	public List<Long> getDoctor() {
 		return doctor;
 	}
 
-	public void setDoctor(String doctor) {
+	public void setDoctor(List<Long> doctor) {
 		this.doctor = doctor;
 	}
 
-	public String getFm() {
+	public List<Long> getFm() {
 		return fm;
 	}
 
-	public void setFm(String fm) {
+	public void setFm(List<Long> fm) {
 		this.fm = fm;
+	}
+
+	public void setHeartRate(List<Integer> heartRate) {
+		this.heartRate = heartRate;
 	}
 
 	public int getInterval() {
 		return interval;
-
-	}
-
-	public void setTime(long time) {
-		this.time = time;
 	}
 
 	public void setInterval(int interval) {
@@ -58,5 +55,9 @@ public class Data {
 
 	public long getTime() {
 		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
 	}
 }
