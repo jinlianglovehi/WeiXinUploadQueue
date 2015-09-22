d661c402da1befa64d734992b5655efb95bcdc13
liuhongjian <liuhongjian@ihealthbaby.cn>
2015-09-22 12:11:21 +0800

    增加dialog工具

--- library/src/main/java/cn/ihealthbaby/weitaixin/library/util/DialogUtil.java
+++ library/src/main/java/cn/ihealthbaby/weitaixin/library/util/DialogUtil.java
@@ -1,13 +1,23 @@
 package cn.ihealthbaby.weitaixin.library.util;
 
-import android.content.Context;
+import android.app.Activity;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 /**
  * Created by liuhongjian on 15/7/23 14:54.
  */
 public class DialogUtil {
+	public static SweetAlertDialog sweetAlertDialog;
 
-	public static void dismissDialog(Context context) {
+	public static SweetAlertDialog CreateDialog(Activity activity) {
+		sweetAlertDialog = new SweetAlertDialog(activity);
+		return sweetAlertDialog;
+	}
 
+	public static void dismissDialog() {
+		if (sweetAlertDialog != null && sweetAlertDialog.isShowing()) {
+			sweetAlertDialog.dismiss();
+		}
 	}
 }
