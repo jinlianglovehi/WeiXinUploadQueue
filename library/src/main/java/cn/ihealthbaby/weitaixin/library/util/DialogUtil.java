40db7035197ce14e0fc6bc6f393ca33b8b1bab25
liuhongjian <liuhongjian@ihealthbaby.cn>
2015-09-22 10:47:21 +0800

    增加DefaultErrorListener对DialogUtil的统一处理

--- library/src/main/java/cn/ihealthbaby/weitaixin/library/util/DialogUtil.java
+++ library/src/main/java/cn/ihealthbaby/weitaixin/library/util/DialogUtil.java
@@ -1,7 +1,13 @@
 package cn.ihealthbaby.weitaixin.library.util;
 
+import android.content.Context;
+
 /**
  * Created by liuhongjian on 15/7/23 14:54.
  */
 public class DialogUtil {
+
+	public static void dismissDialog(Context context) {
+
+	}
 }
