cfe04cfaff5eab717115fee434acf7795e3ad45c
chenweihua <chenweihua@ihbaby.com>
2015-09-22 11:51:12 +0800

    首页界面

--- weitaixin/src/main/java/cn/ihealthbaby/weitaixin/ui/home/HomePageFragment.java
+++ weitaixin/src/main/java/cn/ihealthbaby/weitaixin/ui/home/HomePageFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -27,7 +28,14 @@ public class HomePageFragment extends BaseFragment {
     @Bind(R.id.back) RelativeLayout back;
     @Bind(R.id.title_text) TextView title_text;
     @Bind(R.id.function) TextView function;
+
 //
+
+    @Bind(R.id.llHomeFunctionOneAction) LinearLayout llHomeFunctionOneAction;
+    @Bind(R.id.llHomeFunctionTwoAction) LinearLayout llHomeFunctionTwoAction;
+    @Bind(R.id.llHomeFunctionThreeAction) LinearLayout llHomeFunctionThreeAction;
+    @Bind(R.id.llHomeFunctionFourAction) LinearLayout llHomeFunctionFourAction;
+
 
     private static HomePageFragment instance;
 
@@ -53,17 +61,45 @@ public class HomePageFragment extends BaseFragment {
     }
 
 
-
     @OnClick(R.id.title_text)
     public void title_text() {
         Intent intent=new Intent(getActivity().getApplicationContext(), PayAccountActivity.class);
         startActivity(intent);
     }
 
+
+
+    @OnClick(R.id.llHomeFunctionOneAction)
+    public void llHomeFunctionOneAction() {
+
+    }
+
+
+    @OnClick(R.id.llHomeFunctionTwoAction)
+    public void llHomeFunctionTwoAction() {
+
+    }
+
+
+    @OnClick(R.id.llHomeFunctionThreeAction)
+    public void llHomeFunctionThreeAction() {
+
+    }
+
+
+    @OnClick(R.id.llHomeFunctionFourAction)
+    public void llHomeFunctionFourAction() {
+
+    }
+
+
+
     @Override
     public void onResume() {
         super.onResume();
     }
+
+
 
     @Override
     public void onDestroyView() {
