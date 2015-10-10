76e020a139c14cc2b30e0511878f56147341f16d
liuhongjian <liuhongjian@ihealthbaby.cn>
2015-10-10 14:10:27 +0800

    增加备注说明

--- library/src/main/java/cn/ihealthbaby/weitaixin/library/util/DataStorage.java
+++ library/src/main/java/cn/ihealthbaby/weitaixin/library/util/DataStorage.java
@@ -10,8 +10,20 @@ import cn.ihealthbaby.weitaixin.library.data.bluetooth.data.FHRPackage;
  * Created by liuhongjian on 15/8/12 14:29.
  */
 public class DataStorage {
+	/**
+	 * 胎心数据集合
+	 */
 	public static final List<Integer> fhrs = Collections.synchronizedList(new ArrayList<Integer>());
+	/**
+	 * 胎动数据集合
+	 */
 	public static final List<Integer> fms = Collections.synchronizedList(new ArrayList<Integer>());
+	/**
+	 * 医生干预的数据,指position
+	 */
 	public static final List<Integer> doctors = Collections.synchronizedList(new ArrayList<Integer>());
+	/**
+	 * 存储一个胎心包数据
+	 */
 	public static FHRPackage fhrPackage = new FHRPackage();
 }
