a8b4725b3e0baeb63fca4914775abd42c58f777e
liuhongjian <liuhongjian@ihealthbaby.cn>
2015-10-15 14:20:59 +0800

    修改事件的分类

--- library/src/main/java/cn/ihealthbaby/weitaixin/library/event/MonitorTerminateEvent.java
+++ library/src/main/java/cn/ihealthbaby/weitaixin/library/event/MonitorTerminateEvent.java
@@ -4,11 +4,26 @@ package cn.ihealthbaby.weitaixin.library.event;
  * Created by liuhongjian on 15/9/10 09:52.
  */
 public class MonitorTerminateEvent {
+	/**
+	 * 未知原因
+	 */
+	public static final int EVENT_UNKNOWN = -1;
+	/**
+	 * 自动结束
+	 */
 	public static final int EVENT_AUTO = 1;
-	public static final int EVENT_UNKNOWN = 2;
-	public static final int EVENT_MANUAL = 3;
-	public static final int EVENT_MANUAL_NOT_START = 4;
-	public static final int EVENT_MANUAL_CANCEL = 5;
+	/**
+	 * 手动结束
+	 */
+	public static final int EVENT_MANUAL = 2;
+	/**
+	 * 用户取消,未开始监测
+	 */
+	public static final int EVENT_MANUAL_CANCEL_NOT_START = 3;
+	/**
+	 * 用户取消,已开始监测
+	 */
+	public static final int EVENT_MANUAL_CANCEL_STARTED = 4;
 	private int event;
 
 	public MonitorTerminateEvent() {
