53c9fb66fd8f3a5b393e44799080ab9854086103
chenweihua <chenweihua@ihbaby.com>
2015-10-20 14:38:23 +0800

    微信的id

--- weitaixin/src/main/java/cn/ihealthbaby/weitaixin/net/sourceforge/simcpux/Constants.java
+++ weitaixin/src/main/java/cn/ihealthbaby/weitaixin/net/sourceforge/simcpux/Constants.java
@@ -4,18 +4,18 @@ public class Constants {
 
 
     //appid
-    //请同时修改  androidmanifest.xml里面，.PayActivityd里的属性<data android:scheme="wxb4ba3c02aa476ea1"/>为新设置的appid
-    public static final String APP_ID = "wxf2f565574a968187";
+    //请同时修改  androidmanifest.xml里面，.PayActivityd里的属性<data android:scheme="wx8da8b08e973e9305"/>为新设置的appid
+    public static final String APP_ID = "wx8da8b08e973e9305";
 
 
-
-
-    //商户号
-    public static final String MCH_ID = "1233848001";
-
-
-    //  API密钥，在商户平台设置
-    public static final String API_KEY="412fde4e9c2e2bb619514ecea142e449";
+//
+//
+//    //商户号
+//    public static final String MCH_ID = "1233848001";
+//
+//
+//    //  API密钥，在商户平台设置
+//    public static final String API_KEY="412fde4e9c2e2bb619514ecea142e449";
 
 
 
