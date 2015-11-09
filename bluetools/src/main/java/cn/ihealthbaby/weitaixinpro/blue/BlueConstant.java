6578ec137daabd6f8276970b67c935fa14872183
jinliang <jinlianglovehi@126.com>
2015-11-09 10:14:14 +0800

    蓝牙常量的定义

---
+++ bluetools/src/main/java/cn/ihealthbaby/weitaixinpro/blue/BlueConstant.java
@@ -0,0 +1,28 @@
+package cn.ihealthbaby.weitaixinpro.blue;
+
+/**
+ * Created by jinliang on 15/11/7.
+ */
+public class BlueConstant {
+    //开始播放声音
+   // public static final int stopPlayVoice = 0x01;
+    //直接用方法调用
+    //public static final int adjustVoiceVolumn = 0x02;
+    //停止播放声音
+    //public static final int startPlayVoice = 0x03;
+
+    //开始语音写入语音文件
+    public static final int startWriteVoiceFile = 0x04;
+    //停止写入语音
+    public static final int stopWriteVoiceFile = 0x05;
+
+    // 开始记录心率
+    public static final int  startRecord=0x06;
+    // 停止记录心率
+    public static final int  endRecord =0x07 ;
+    //开始保存语音的文件
+//    public static final int  startSaveVoiceFile =0x08;
+//    //不保存语音文件
+//    public static final int   unSaveVoiceFile =0x09;
+
+}
